108e287abc5c0178f5a2d2f927917c5143d8da49
Peter Lucido <petermlucido@gmail.com>
2023-04-13 16:48:05 -0500

    solve three

--- models/skill.js
+++ models/skill.js
@@ -3,13 +3,13 @@ import mongoose from 'mongoose'
 // optional shortcut to the mongoose.Schema class
 const Schema = mongoose.Schema
 
-const todoSchema = new Schema({
+const skillSchema = new Schema({
   text: String,
   have: Boolean,
 })
 
 // Compile the schema into a model and export it
-const Skill = mongoose.model('Skill', todoSchema)
+const Skill = mongoose.model('Skill', skillSchema)
 
 export {
   Skill
