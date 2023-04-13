74846e70b475cb114c8a4b9d6df17943673d4ad1
Peter Lucido <petermlucido@gmail.com>
2023-04-13 13:34:05 -0500

    moving on to section two

---
+++ data/skill-data.js
@@ -0,0 +1,18 @@
+const skills = [
+  {text: 'Back Flip', have: true, _id: 125223},
+  {text: 'Speak Spanish', have: false, _id: 125223},
+  {text: 'Front Flip', have: true, _id: 125223},
+  {text: 'Skateboarding', have: true, _id: 125223},
+  {text: 'BMX', have: true, _id: 125223},
+  {text: 'Fishing', have: true, _id: 125223},
+  {text: 'Soccer', have: true, _id: 125223},
+  {text: 'Moon-walking', have: true, _id: 125223},
+  {text: 'Paper Airplane Making', have: true, _id: 125223},
+  {text: 'Orange Pealing', have: true, _id: 125223},
+  {text: 'SuperMoto', have: true, _id: 125223},
+  {text: 'Mechanical', have: true, _id: 125223},
+]
+
+export { 
+	skills
+}
---
+++ routes/skills.js
@@ -0,0 +1,17 @@
+import { Router } from 'express'
+
+import * as skillsCtrl from '../controllers/skills.js'
+
+const router = Router()
+// GET localhost:3000/skills
+router.get('/', skillsCtrl.index)
+// GET localhost:3000/skills/new
+// router.get('/new', skillsCtrl.new)
+// GET localhost:3000/skills/:skillId
+// router.get('/:skillId', skillsCtrl.show)
+// POST localhost:3000/skills
+// router.post('/', skillsCtrl.create)
+// DELETE localhost:3000/skills/:skillId
+// router.delete('/:skillId', skillsCtrl.delete)
+
+export { router }
